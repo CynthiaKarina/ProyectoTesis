b4f8dff4d2688d75ee8ef7f3a1d0c6af44f8b578
Cynthia Karina <cynthiakarina.ing@hotmail.com>
2025-08-22 11:51:12 -0600

    Mensaje describiendo los cambios

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     SECRET_KEY = 'tu_clave_secreta_aqui'
     # Configuración actualizada para Google Cloud SQL
     # Instancia: seismic-sentry-466506-j0-northamerica-south1-proyecto-tesis
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://proyecto-tesis:K7re^bY0q":Rf*|R@34.51.57.50:3306/proyectotesis'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/proyectotesis'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://proyecto-tesis:K7re^bY0q":Rf*|R@34.51.57.50:3306/proyectotesis'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/proyectotesis'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Configuración para subida de archivos
@@ -19,8 +19,8 @@ class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', '1', 'yes']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  # ej. notificaciones.sigral@gmail.com
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  # Usa contraseña de aplicación de Gmail
+    MAIL_USERNAME = os.environ.get('notificaciones.sigral@gmail.com')  # ej. notificaciones.sigral@gmail.com
+    MAIL_PASSWORD = os.environ.get('gyzz yjnp glxz qzab')  # Usa contraseña de aplicación de Gmail
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', MAIL_USERNAME)
 
     # Salt para tokens de email (restablecer contraseña, etc.)
